1d3a332193e30b652b29919bf792617a4a234e2b
AbdulkhaleqAmin <53509953+asuhbani@users.noreply.github.com>
2023-02-20 23:21:31 +0300

    Add files via upload

---
+++ final_assignment_DS_Tools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # **My Jupyter Notebook on IBM Watson Studio**
+
+# ### Abdulkhaleq Amin
+
+# 100% Human
+
+# *I am interested in data science because it's the future.*
+
+# ### This Code Add Two Numbers
+
+# In[1]:
+
+
+def add(a,b):
+    print(a+b)
+add(5,3)
+
+
+# <a href="https://www.linkedin.com/in/abdulkhaleq-al-suhbani-b925b4229">Eng. Abdulkhaleq Amin</a>
+
+# | Experiance     | Skills                   |
+# | -------------- | ------------------------ |
+# | Data Analytics | Attention to Details     |
+# | IMO            | Statistics               |
+
+# Languages:
+# 1) Arabic
+# 2) English
